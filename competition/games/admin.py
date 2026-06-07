ee6b10217df459ad35f4eb372c31d29ed79aad00
Parisa Asoodeh <parisaasoodeh70@gmail.com>
2026-06-07 20:15:41 +0430

    record models in admin panel

--- competition/games/admin.py
+++ competition/games/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Team, TeamMembership
 
-# Register your models here.
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'captain', 'total_score')
+
+@admin.register(TeamMembership)
+class TeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'team', 'individual_score')
