3099899d0ba54765e68bb8139c617b24ba646315
Parisa Asoodeh <parisaasoodeh70@gmail.com>
2026-06-15 17:40:55 +0430

    continue refactoring

--- competition/teams/admin.py
+++ competition/teams/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Team, TeamMembership
 
-admin.site.register(Team)
-admin.site.register(TeamMembership)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'captain', 'total_score', 'created_at')
+    search_fields = ('name', 'captain__username')
+    list_filter = ('created_at',)
+
+
+@admin.register(TeamMembership)
+class TeamMembershipAdmin(admin.ModelAdmin):
+    list_display = ('user', 'team', 'individual_score')
+    search_fields = ('user__username', 'team__name')
